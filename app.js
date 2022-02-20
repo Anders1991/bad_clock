b36f03dd695cc4eff1b8ef0e5344f9f0414b555d
Anders Lauridsen <58211596+Anders1991@users.noreply.github.com>
2022-02-20 22:16:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// import Express
+const { json } = require("express");
+const express = require("express");
+
+// instantiated instance
+const app = express();
+
+// allow us to parse json
+app.use(express.json());
+
+//Index
+app.get("/", (req, res) => {
+    
+    res.sendFile('index.html',{root:'public'});
+
+});
+
+app.listen(8080);
